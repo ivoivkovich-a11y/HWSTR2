d34701858a041df96c9409ff55d86d7d4fe78b8a
ivoivkovich <ivoivkovich@gmail.com>
2026-04-13 09:35:30 +0300

    HWSTR2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    {
+        {
+            // Задача №1
+            System.out.println("Задача №1");
+            String firstName = "Ivanov";
+            String middleName = "Ivan";
+            String lastName = "Ivanovich";
+            String fullName = firstName + " " + middleName + " " + lastName;
+            System.out.println("fullName = " + fullName);
 
-
+            // Задача №2
+            System.out.println("Задача №2");
+            String fullNameUppedrCase = fullName.toUpperCase();
+            System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUppedrCase);
+        }
+        {
+            // Задача №3
+            System.out.println("Задача №3");
+            String fullName = "Иванов Семён Семёнович";
+            String fullNameNew = fullName.replace("ё","е");
+            System.out.println("Данные Ф. И. О. сотрудника —" + fullNameNew);
+        }
+    }
 }
